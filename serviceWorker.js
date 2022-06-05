a29aca8633b621330d3e23fe48ee6ffc2d7c92eb
cosmetics-id-site <glamarycosmetics@gmail.com>
2022-06-05 09:06:13 +0700

    first commit

---
+++ serviceWorker.js
@@ -0,0 +1,34 @@
+const staticBlogAndre = "blog-andresite-v1"
+const assets = [
+  "/",
+  "/PWA-Blog/",
+  "/assets/css/blog-andre.css",
+  "/assets/css/blog-andre-2.css",
+  "/assets/js/blog-andre.js",
+  "/images/coffee1.jpg",
+  "/images/coffee2.jpg",
+  "/images/coffee3.jpg",
+  "/images/coffee4.jpg",
+  "/images/coffee5.jpg",
+  "/images/coffee6.jpg",
+  "/images/coffee7.jpg",
+  "/images/coffee8.jpg",
+  "/images/coffee9.jpg",
+]
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticBlogAndre).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
+
+
+self.addEventListener("fetch", fetchEvent => {
+    fetchEvent.respondWith(
+      caches.match(fetchEvent.request).then(res => {
+        return res || fetch(fetchEvent.request)
+      })
+    )
+  })
