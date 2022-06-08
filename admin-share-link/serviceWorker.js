deafedc55480f2fb68098b92602a108ac22b4514
cosmetics-id-site <glamarycosmetics@gmail.com>
2022-06-08 23:34:21 +0700

    first commit

---
+++ admin-share-link/serviceWorker.js
@@ -0,0 +1,26 @@
+const staticDevCoffee = "dev-coffee-site-v1"
+const assets = [
+  "/",
+  "../../index.html",
+  "../../assets/css/styles.css",
+  "../../assets/js/script.js",
+  "../../assets/js/app.js",
+  "/images/coffee1.jpg",
+]
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticDevCoffee).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
+
+
+self.addEventListener("fetch", fetchEvent => {
+    fetchEvent.respondWith(
+      caches.match(fetchEvent.request).then(res => {
+        return res || fetch(fetchEvent.request)
+      })
+    )
+  })
