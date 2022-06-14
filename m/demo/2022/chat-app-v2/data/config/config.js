143fffaa0db5bc294a2be00e2282a357939fdf04
cosmetics-id-site <glamarycosmetics@gmail.com>
2022-06-14 23:09:32 +0700

    first commit

---
+++ m/demo/2022/chat-app-v2/data/config/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBmiZlEop13KU-NpS2TT8NZGzTtOWPuEKY",
+    authDomain: "solityid-demo.firebaseapp.com",
+    databaseURL: "https://solityid-demo-default-rtdb.firebaseio.com",
+    projectId: "solityid-demo",
+    storageBucket: "solityid-demo.appspot.com",
+    messagingSenderId: "35437928940",
+    appId: "1:35437928940:web:6fb326a27f28effefd20c9",
+    measurementId: "G-XWY7J7EV3Z"
+};
+const app = firebase.initializeApp(firebaseConfig);
---
+++ m/demo/2022/chat-app-v3/Scripts/firebase.init.js
@@ -0,0 +1,13 @@
+﻿// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBmiZlEop13KU-NpS2TT8NZGzTtOWPuEKY",
+    authDomain: "solityid-demo.firebaseapp.com",
+    databaseURL: "https://solityid-demo-default-rtdb.firebaseio.com",
+    projectId: "solityid-demo",
+    storageBucket: "solityid-demo.appspot.com",
+    messagingSenderId: "35437928940",
+    appId: "1:35437928940:web:6fb326a27f28effefd20c9",
+    measurementId: "G-XWY7J7EV3Z"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
